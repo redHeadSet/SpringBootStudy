e1fa2d70fa66583447eb2079568d758242bc036d
redHeadSet <stikfascube@gmail.com>
2021-07-08 20:40:49 +0900

    library_practice

---
+++ library_practice/src/main/java/hjjung_test/library_practice/LibraryPracticeApplication.java
@@ -0,0 +1,13 @@
+package hjjung_test.library_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryPracticeApplication.class, args);
+	}
+
+}
---
+++ library_practice/src/test/java/hjjung_test/library_practice/LibraryPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package hjjung_test.library_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
